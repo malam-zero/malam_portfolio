54a16bad7ca64f744bd6e103b5ad1206a5fb3eab
watch-r <nabil202051@gmail.com>
2026-06-08 07:18:21 +0600

    Add text style definitions for SmallTextStyles and LargeTextStyles

--- lib/app_text_styles.dart
+++ lib/app_text_styles.dart
@@ -2,4 +2,61 @@ import 'package:flutter/material.dart';
 
 abstract class AppTextStyle {
   TextStyle get titleSmBold;
+  TextStyle get bodyMdMedium;
+  TextStyle get titleLgBold;
+  TextStyle get titleMdMedium;
+  TextStyle get bodyLgBold;
+  TextStyle get bodyLgMedium;
+}
+
+class SmallTextStyles extends AppTextStyle {
+  @override
+  TextStyle get bodyLgBold =>
+      const TextStyle(fontSize: 14, fontWeight: FontWeight.bold);
+
+  @override
+  TextStyle get bodyLgMedium =>
+      const TextStyle(fontSize: 14, fontWeight: FontWeight.w500);
+
+  @override
+  TextStyle get bodyMdMedium =>
+      const TextStyle(fontSize: 12, fontWeight: FontWeight.w500);
+
+  @override
+  TextStyle get titleLgBold =>
+      const TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
+
+  @override
+  TextStyle get titleMdMedium =>
+      const TextStyle(fontSize: 14, fontWeight: FontWeight.w500);
+
+  @override
+  TextStyle get titleSmBold =>
+      const TextStyle(fontSize: 12, fontWeight: FontWeight.bold);
+}
+
+class LargeTextStyles extends AppTextStyle {
+  @override
+  TextStyle get bodyLgBold =>
+      const TextStyle(fontSize: 16, fontWeight: FontWeight.bold);
+
+  @override
+  TextStyle get bodyLgMedium =>
+      const TextStyle(fontSize: 16, fontWeight: FontWeight.w500);
+
+  @override
+  TextStyle get bodyMdMedium =>
+      const TextStyle(fontSize: 14, fontWeight: FontWeight.w500);
+
+  @override
+  TextStyle get titleLgBold =>
+      const TextStyle(fontSize: 40, fontWeight: FontWeight.bold);
+
+  @override
+  TextStyle get titleMdMedium =>
+      const TextStyle(fontSize: 32, fontWeight: FontWeight.w500);
+
+  @override
+  TextStyle get titleSmBold =>
+      const TextStyle(fontSize: 24, fontWeight: FontWeight.bold);
 }
