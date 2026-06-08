559365022b61e2d9f93e2e3a4a71d652118c67e8
malam-zero <mahmudalam.zero@outlook.com>
2026-06-08 11:14:28 +0600

    Fix formatting in main.dart by adding a space after AppLocalizations.delegate

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       localizationsDelegates: [
-        AppLocalizations.delegate,
+        AppLocalizations.delegate, 
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
